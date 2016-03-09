50560779df63262d7974f31c96a3c24c3559375c
Hope Davita <fatblacklip@mail.com>
2016-03-09 13:18:43 -0600

    Update slither.js
    Completely reworked

--- slither.js
+++ slither.js
@@ -1,205 +1,109 @@
-$(document).ready(function(){
-	begin(5);
-});
-
-var activeCells;
-function begin(size){
-	createGrid(size, size);
-	deactivateEdges();
-	displayGrid(size, size ,40);
-
-	activeCells = (size - 2)* (size - 2); // setup
-};
-
-//capitalized to designate it as constructor
-var Cell = function(x,y){
-	this.xCoordinate = x;
-	this.yCoordinate = y;
-	this.type; // edge for deactivated
-	this.playable = true;
-	this.value;
-};
-
-var cell; //use arr over obj for indexing
-function createGrid(width, height){ //the same for this purpose
-	cell = [];
-	for(x=0; x<width; x++){  //for every column
-		cell[x] = [];
-
-		for(y=0; y<height; y++){ //for every item in the column
-			cell[x][y] = new Cell(x,y); //create a cell
-		};
+function findAllPaths(x,y, size){
+	function Cell(x,y){
+		this.xCoordinate = x;
+		this.yCoordinate = y;
+		this.valid = true;
+		this.value;
 	};
-	return cell;
-};
-
-function deactivateEdges(){
-	var width = cell.length;
-	var height = cell[0].length;
-	var results = [];
-	for(x=0; x<width; x++){
-		for(y=0; y<height; y++){
-			var test = cell[x][y];
-
-			if (test.xCoordinate === 0 || test.xCoordinate === (width - 1)){
-				test.playable = false;
-				results.push([test.xCoordinate , test.yCoordinate]);
-
-			} else if(test.yCoordinate === 0 || test.yCoordinate === (height - 1)){
-				test.playable = false;
-				results.push([test.xCoordinate , test.yCoordinate]);
+	function buildGrid(size){
+	size = size + 2 /*edge cells*/
+	var grid = [];
+		for(i=0;i<size;i++){
+			grid[i]= [];
+			for(j=0;j<size;j++){
+				grid[i][j] = new Cell(i,j);
+			}
+		}
+		return grid;
+	};
+	var grid = buildGrid(size);
+	for(i=0;i<grid.length;i++){  //make edges invalid
+		grid[0][i].valid = false;
+		grid[i][0].valid = false;
+		grid[grid.length -1][i].valid = false;
+		grid[i][grid.length -1].valid = false;
+	};
+	var start = grid[x][y];
+	var allPaths = [];
+	if (start.valid){
+		findPath();
+		function findPath(){
+			var path = [];
+			function findAdjacent(x,y,shift){  //shift is optional
+				var adjacentCells = [
+					grid[x][y +1],    // N
+					grid[x +1][y +1], // NE
+					grid[x +1][y],    // E
+					grid[x +1][y -1], // SE
+					grid[x][y -1],    // S
+					grid[x -1][y -1], // SW
+					grid[x -1][y],    // W
+					grid[x -1][y +1]  // NW
+					];
+				for(i=0; i<shift; i++){
+					adjacentCells.push(adjacentCells.shift());
+				};
+				return adjacentCells;
 			};
-		};
-	};
-	return results;
-};
-function displayGrid (width, height, cellSize){
-	function createElements(){
-		$(".puzzle").empty();
-		for(y=0; y<height; y++){
-			$(".puzzle").prepend("<div class='row row" + y + "'></div>");
-			for(x=0;x<width;x++){
-				$(".row" + y).append("<div class='cell cell" + x + "-" + y + "'></div>");
+			nextStep(start, 0);
+			function nextStep(start, shift){
+				console.log("start: " + start.xCoordinate + "," + start.yCoordinate);
+				console.log("with shift " + shift);
+
+				start.valid = false;
+				var testArray = findAdjacent(start.xCoordinate, start.yCoordinate, shift);
+				for(i=0; i<testArray.length; i++){
+					var test = testArray[i];
+					if(test.valid === true){
+						path.push(start);
+						return nextStep(test, shift); // to 0
+					}
+				};
+				console.log("no hits at " + start.xCoordinate, start.yCoordinate);
+
+				if(path.length === (size*size)-1){
+					path.push(start);
+					return path;
+
+				} else if(shift < testArray.length-1){  //if already shifted 7 times
+					start.valid = true;
+
+					return nextStep(path.pop(), shift+1);
+
+				}else{
+					console.log("jump back another")
+					return path;
+				}
+			}
+			console.log(path);
+			for(i=0;i<path.length;i++){
+				path[i].value = i + 1;
 			};
-		};
-	};
-	function sizeGrid(){
-		if (cellSize === undefined) cellSize = 30; //so cellSize is optional
-		$(".row").css({
-			"width": width*cellSize,
-			"height": cellSize});
-		$(".cell").css({
-			"width": cellSize,
-			"height": cellSize});
-	};
-	function turnGrey(){
-		for(i=0;i< deactivateEdges().length;i++){  //for every deactivated result
-			var x = deactivateEdges()[i][0];
-			var y = deactivateEdges()[i][1];
-			$(".cell"+ x + "-" + y).css("background-color", "#BAB7BD");
-		};
-	};
-	createElements();
-	sizeGrid();
-	turnGrey();
+			allPaths.push(path);
+		}
+		return allPaths;
+	} else {return null}
 };
 
-function pathValues(){              //just for display
+/* allPaths will currently only contain one path. 
+display(findAllPaths(x,y,size)[0], gridSize)
+Eventually: for every item in allPaths, display(path) */
+
+function display(path,gridSize){
+	$(".puzzle").empty();
+	$(".puzzle").css("width", gridSize*40); //temp
+
+	for(y=0; y<gridSize; y++){
+		$(".puzzle").prepend("<div class='row row" + y + "'></div>");
+		for(x=0;x<gridSize;x++){
+			$(".row" + y).append("<div class='cell column" + x + " cell" + x + "-" + y + "'></div>");
+		};
+	};
 	for(i=0;i<path.length;i++){
-		path[i].value = i + 1;
 		x = path[i].xCoordinate;
 		y = path[i].yCoordinate;
 		$('.cell' + x + "-" + y).empty();
 		$('.cell' + x + "-" + y).append(path[i].value);
-	};
-};
-
-function turnPurple(x,y){
-	$(".cell"+ x + "-" + y).css("background-color", "#8C60BD");  //temp for learning.
-};
-function undoPurple(x,y){
-	$(".cell"+ x + "-" + y).css("background-color", "#C8F4D0");  //temp for learning.
-};
-
-function clearGrid(){
-	deactivateEdges();
-	for(i=1;i<(cell.length-1);i++){
-		for(j=1;j<(cell[i].length-1);j++){
-
-			var targetCell = cell[i][j];
-			targetCell.playable = true;
-			targetCell.value = undefined;
-
-			$('.cell' + targetCell.xCoordinate + "-" + targetCell.yCoordinate).empty();
-			undoPurple(j,i);
-		};
-	};
-	return cell;
-};
-
-var allPaths;
-var path;
-
-function findAllPaths(x,y){ //arg- start cell
-
-	allPaths = [];
-	clearGrid();
-
-	if (cell[x][y] !== undefined && cell[x][y].playable){  //if start is playable
-		
-		findPath(x,y);
-		pathValues();  //for display
-
-		return allPaths;
-
-	} else return null;
-};
-
-function findPath(x,y){
-
-	path = [];
-	clearGrid();
-	nextCell(x,y);
-};
-
-function nextCell(x, y){
-	var start = cell[x][y];
-
-	var	adjacentCells = [
-		cell[x][y +1],    // N
-		cell[x +1][y +1], // NE
-		cell[x +1][y],    // E
-		cell[x +1][y -1], // SE
-		cell[x][y -1],    // S
-		cell[x -1][y -1], // SW
-		cell[x -1][y],    // W
-		cell[x -1][y +1]  // NW
-		];
-	
-	for(i=0;i<adjacentCells.length;i++){  //goes through every adj until one is playable
-
-		var test = adjacentCells[i];
-		if (test.playable === true){          //when one is playable, it starts again at that spot
-
-			path.push(start);
-			start.playable = false;
-			turnPurple(x,y);                  //just for display
-
-			return nextCell(test.xCoordinate, test.yCoordinate);	
-		} else {
-			continue     //if test space is not playable, it continues clockwise
-		};
-	};
-
-    //if function gets to this point, then none of the adjacent spaces were playable
-
-
-	if (path.length === activeCells-1){
-		path.push(start);
-		start.playable = false;
-		turnPurple(x,y);   //display
-
-		allPaths.push(path);
-
-	} else {  // trying to go back to the last space and try a different adj cell
-
-		console.log("You got stuck at " + start.xCoordinate + "," + start.yCoordinate);
-
-		start = path[path.length - 1];
-
-		console.log("Let's go back to " + start.xCoordinate + "," + start.yCoordinate);
-
-		adjacentCells.push(adjacentCells.shift());
-
-		console.log(adjacentCells);
-		console.log(start.xCoordinate, start.yCoordinate);
-
-//		nextCell(start.xCoordinate, start.yCoordinate);
-		 
 	}
-};
-
-
-
-
-
+	$(".column0, .column" + (gridSize-1) + ", .row0 * , .row" + (gridSize-1) + " *" ).css("background-color", "#7F7E8C");
+}
