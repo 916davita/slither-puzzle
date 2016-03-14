350e8055fcf58b5a84079604af9ef56f324091a5
Hope Davita <fatblacklip@mail.com>
2016-03-13 22:27:08 -0500

    Update slither.js
    All paths for every start point.

--- slither.js
+++ slither.js
@@ -1,87 +1,110 @@
-function findAllPaths(x,y, size){
-    function Cell(x,y){
-        this.xCoordinate = x;
-        this.yCoordinate = y;
-        this.valid = true;
-        this.value;
-        this.testShift = 0;
-    }
-    function buildGrid(size){
-        size = size + 2 // edge cells
-        var grid = [];
-        for(i=0;i<size;i++){
-            grid[i]= [];
-            for(j=0;j<size;j++){
-                grid[i][j] = new Cell(i,j);
-            }
-        }
-        for(i=0;i<grid.length;i++){  //make edges invalid
-            grid[0][i].valid = false;
-            grid[i][0].valid = false;
-            grid[grid.length -1][i].valid = false;
-            grid[i][grid.length -1].valid = false;
-        };
-        return grid;
-    }
-    function findAdjacent(current){
-        var x = current.xCoordinate;
-        var y = current.yCoordinate;
-        var adjacentCells = [
-            grid[x][y +1],    // N
-            grid[x +1][y +1], // NE
-            grid[x +1][y],    // E
-            grid[x +1][y -1], // SE
-            grid[x][y -1],    // S
-            grid[x -1][y -1], // SW
-            grid[x -1][y],    // W
-            grid[x -1][y +1]  // NW
-            ];
-        return adjacentCells
-    }
-    function clonePath(path){
-        var newArray = [];
-        for(i=0;i<path.length;i++){
-            var x = path[i].xCoordinate;
-            var y = path[i].yCoordinate;
-            var clone = new Cell(x,y);
-            clone.value = i;
-            clone.valid = false;
-            clone.testShift = path[i].testShift;
-            newArray.push(clone);
-        } return newArray;
-    }
-    var allPaths = [];
-    var currentPath = [];
-    
-    function makePath(currentCell){
-        do{currentCell = takeStep(currentCell);
-        console.log(currentCell.xCoordinate + "," + currentCell.yCoordinate);
-        }while(currentPath.length !== size*size -1); //will stop looping when currentPath is full length
-        currentPath.push(currentCell);
-        allPaths.push(clonePath(currentPath));
-    }
-    function takeStep(currentCell){
-        currentCell.valid = false;
-        var testArray = findAdjacent(currentCell);
-        for(i=currentCell.testShift;i<(8 - currentCell.testShift);i++){
-            if(testArray[i].valid){
-                currentCell.testShift = i+1;
-                currentPath.push(currentCell); //must be after testShift is altered
-                return testArray[i];
-            }
-        }
-        // at this point, no adj were hits
-        currentCell.valid = true;
-        currentCell.testShift = 0;
-        return takeStep(currentPath.pop()); //recur until a single hit is found
-    }
-    var grid = buildGrid(size);
-    makePath(grid[x][y]);
-
-    console.log(currentPath);
-    return allPaths;
+function Cell(x,y){
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+    this.valid = true;
+    this.value;
+    this.testShift = 0;
 }
-function display(path,gridSize, puzzleId){
+function buildGrid(size){
+    size = size + 2;
+    var grid = [];
+    for(i=0;i<size;i++){
+        grid[i]= [];
+        for(j=0;j<size;j++){
+            grid[i][j] = new Cell(i,j);
+        }
+    }
+    for(i=0;i<grid.length;i++){
+        grid[0][i].valid = false;
+        grid[i][0].valid = false;
+        grid[grid.length -1][i].valid = false;
+        grid[i][grid.length -1].valid = false;
+    };
+    return grid;
+}
+function findAdjacent(current){
+    var x = current.xCoordinate;
+    var y = current.yCoordinate;
+    var adjacentCells = [
+        grid[x][y +1],    // N
+        grid[x +1][y +1], // NE
+        grid[x +1][y],    // E
+        grid[x +1][y -1], // SE
+        grid[x][y -1],    // S
+        grid[x -1][y -1], // SW
+        grid[x -1][y],    // W
+        grid[x -1][y +1]  // NW
+        ];
+    return adjacentCells
+}
+function clonePath(path){
+    var newArray = [];
+    for(i=0;i<path.length;i++){
+        var x = path[i].xCoordinate;
+        var y = path[i].yCoordinate;
+        var clone = new Cell(x,y);
+        clone.value = i;
+        clone.valid = false;
+        clone.testShift = path[i].testShift;
+        newArray.push(clone);
+    } return newArray;
+}
+function makePath(currentCell, size){
+    do{ var currentCell = takeStep(currentCell);
+    } while(currentPath.length !== (size*size) -1);
+    currentPath.push(currentCell);
+    allPaths.push(clonePath(currentPath));
+}
+function takeStep(currentCell){
+    currentCell.valid = false;
+    var testArray = findAdjacent(currentCell);
+    for(i=currentCell.testShift;i<8;i++){
+        if(testArray[i].valid){
+            currentCell.testShift = i+1;
+            currentPath.push(currentCell);
+            return testArray[i];
+        }
+    }
+    reset(currentCell);
+    return takeStep(currentPath.pop());
+}
+
+var grid, currentPath, allPaths;
+function findAllPaths(x,y, size){
+    allPaths = [];
+    currentPath = [];
+    grid = buildGrid(size);
+    makePath(grid[x][y], size);
+
+    var currentCell;
+    function removeCells(num){ //num is optional
+        for(var l=0;l<num;l++){ //for every cell to be removed
+            currentCell = currentPath[currentPath.length-(l+1)];
+            reset(currentCell);
+
+        console.log("reset cell");
+            console.log(currentCell);
+        }
+        currentPath.splice(-num, num);
+        console.log(currentPath);
+
+        currentCell = currentPath[currentPath.length-1]; //setup for next start
+        currentCell.valid = true;
+        currentCell = currentPath.pop();
+    }
+    for(n=0;n<137;n++){
+        console.log(n);
+        removeCells();
+        makePath(currentCell, size);
+    }
+    console.log(allPaths);
+    return allPaths; 
+}
+function reset(targetCell){
+    targetCell.valid = true;
+    targetCell.testShift = 0;
+}
+function display(path, gridSize, puzzleId){
     $(".puzzle-container").append("<div class='puzzle puzzle" + puzzleId + "'></div>");
     $(".puzzle" + puzzleId).css("width", gridSize*40); //temp
 
@@ -100,8 +123,8 @@ function display(path,gridSize, puzzleId){
 }
 function displayAllPaths(x,y,size){
     var allPaths= findAllPaths(x,y,size);
-    for(i=0;i<allPaths.length;i++){
-        display(allPaths[i], size +2, i)
+    for(var h=0;h<allPaths.length;h++){
+        display(allPaths[h], size +2, h)
     }
 }
 
