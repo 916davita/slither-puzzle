6103004d00f68bcc0592a38258d4b7afb13297af
Hope Davita <fatblacklip@mail.com>
2016-05-17 13:22:43 -0500

    Externalized takeStep() recursion

--- find_solutions.js
+++ find_solutions.js
@@ -1,71 +1,55 @@
-function mongoIfy(solution_JSONs){
-    // retrieve
-    var MongoClient = require('mongodb').MongoClient;
-    // Connect to the db
-    MongoClient.connect("mongodb://localhost:27017/slither_puzzle", function(err, db) {
-      if(err) { return console.dir(err); }
-      if(!err) {
-        console.log("We are connected");
-
-        var collection = db.collection('solutions');
-
-        collection.insert(solution_JSONs)
-      }
-    });
+function Cell(x,y){
+    this.xCoordinate = x;
+    this.yCoordinate = y;
+    this.valid = true;
+    this.value;
+    this.testShift = 0;
 }
-
-function findAllPaths(x,y, size){
-    function Cell(x,y){
-        this.xCoordinate = x;
-        this.yCoordinate = y;
-        this.valid = true;
-        this.value;
-        this.testShift = 0;
-    }
-    function buildGrid(size){
-        size = size + 2;
-        var grid = [];
-        for(i=0;i<size;i++){
-            grid[i]= [];
-            for(j=0;j<size;j++){
-                grid[i][j] = new Cell(i,j);
-            }
+function buildGrid(size){
+    size = size + 2;
+    var grid = [];
+    for(i=0;i<size;i++){
+        grid[i]= [];
+        for(j=0;j<size;j++){
+            grid[i][j] = new Cell(i,j);
         }
-        for(i=0;i<grid.length;i++){
-            grid[0][i].valid = false;
-            grid[i][0].valid = false;
-            grid[grid.length -1][i].valid = false;
-            grid[i][grid.length -1].valid = false;
-        };
-        return grid;
     }
-    function findAdjacent(current){
-        var x = current.xCoordinate;
-        var y = current.yCoordinate;
-        var adjacentCells = [
-            grid[x][y +1],    // N
-            grid[x +1][y +1], // NE
-            grid[x +1][y],    // E
-            grid[x +1][y -1], // SE
-            grid[x][y -1],    // S
-            grid[x -1][y -1], // SW
-            grid[x -1][y],    // W
-            grid[x -1][y +1]  // NW
-            ];
-        return adjacentCells
+    for(i=0;i<grid.length;i++){
+        grid[0][i].valid = false;
+        grid[i][0].valid = false;
+        grid[grid.length -1][i].valid = false;
+        grid[i][grid.length -1].valid = false;
+    };
+    return grid;
+}
+function findAdjacent(current, grid){
+    var x = current.xCoordinate;
+    var y = current.yCoordinate;
+    var adjacentCells = [
+        grid[x][y +1],    // N
+        grid[x +1][y +1], // NE
+        grid[x +1][y],    // E
+        grid[x +1][y -1], // SE
+        grid[x][y -1],    // S
+        grid[x -1][y -1], // SW
+        grid[x -1][y],    // W
+        grid[x -1][y +1]  // NW
+        ];
+    return adjacentCells
+}
+function reset(targetCell){
+    targetCell.valid = true;
+    targetCell.testShift = 0;
+}
+function slimData(path){
+    var pathString = '';
+    for(i=0;i<path.length;i++){ //x and y each always one digit
+        pathString += path[i].xCoordinate;
+        pathString += path[i].yCoordinate;
     }
-    function reset(targetCell){
-        targetCell.valid = true;
-        targetCell.testShift = 0;
-    }
-    function slimData(path){
-        var pathString = '';
-        for(i=0;i<path.length;i++){ //x and y each always one digit
-            pathString += path[i].xCoordinate;
-            pathString += path[i].yCoordinate;
-        }
-        return pathString;
-    }
+    return pathString;
+}
+function findAllPaths(x,y, size, db, collection){
     function makePath(currentCell, size){
         do{ var currentCell = takeStep(currentCell);
         } while(currentPath.length !== (size*size) -1);
@@ -75,33 +59,38 @@ function findAllPaths(x,y, size){
         }
         currentPath.push(currentCell);
         var slim = slimData(currentPath);
-        solutions.push({
+        collection.insert({
             "coordinates" : slim,
             "solution_id" : solution_id});
-
         solution_id++
     }
     function takeStep(currentCell){
         if(currentCell !== undefined){ //for testing
-            currentCell.valid = false;
-            var testArray = findAdjacent(currentCell);
-            for(i=currentCell.testShift;i<8;i++){
-                if(testArray[i].valid){
-                    currentCell.testShift = i+1;
-                    currentPath.push(currentCell);
-                    return testArray[i];
+            function sTeP(){
+                currentCell.valid = false;
+                var testArray = findAdjacent(currentCell, grid);
+
+                for(i=currentCell.testShift;i<8;i++){
+                    if(testArray[i].valid){
+                        currentCell.testShift = i+1;
+                        currentPath.push(currentCell);
+                        return testArray[i];
+                    }
                 }
+                reset(currentCell);
+                currentCell = currentPath.pop();
+                return null;
             }
-            reset(currentCell);
-            return takeStep(currentPath.pop());
+            do{var result = sTeP(currentCell)}
+            while(result === null);
+            return result;
         } else {
             currentPath.push(new Cell(-1,-1)); //for testing
             return
         }
     }
-    var grid, currentPath, solutions, solution_id;
+    var grid, currentPath, solution_id;
     solution_id = 1;
-    solutions = [];
     currentPath = [];
     grid = buildGrid(size);
     makePath(grid[x][y], size);
@@ -119,14 +108,25 @@ function findAllPaths(x,y, size){
         currentCell = currentPath.pop();
     }
 
-    for(n=1;n<60000000;n++){  //start at 1 because one is already complete
+    for(n=1;n<100;n++){  //start at 1 because one is already complete
         removeCells();
         if(makePath(currentCell, size) === null){ //for testing
             break
         }
     }
-    return solutions; 
+    return;
 }
-var solutions = findAllPaths(1,1,5);
-mongoIfy(solutions);
+function main(){
+    var MongoClient = require('mongodb').MongoClient;
 
+    MongoClient.connect("mongodb://localhost:27017/slither_puzzle", function(err, db) {
+      if(err) { return console.dir(err); }
+      if(!err) {
+        console.log("We are connected");
+        var collection = db.collection('solutions');
+        findAllPaths(1,1,5, db, collection);
+      }
+    });
+    return
+}
+main()
